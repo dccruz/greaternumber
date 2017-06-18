5206f5162fe0a5f9b6f0c53a2f0c20404e97162e
DC Cruz <dccoder@icloud.com>
2017-06-18 08:51:17 -0700

    Which number is greater

---
+++ Which number is greater/Which number is greater/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+
+cout << "DC Cruz June 15th 2017"<<endl;
+int firstNum = 0, secondNum = 0;
+cout << "Enter first Number (example 69): " << endl;
+cin >> firstNum;
+cout << "Enter second Number (example 42): " << endl;
+cin >> secondNum;
+    if ( firstNum < secondNum )
+    { cout << "First number is less then second number " << endl; }
+    else
+    { cout << "First number is greater than or equal to second number " << endl;
+return 0;
+    }}
